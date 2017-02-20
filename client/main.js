afa6b8131e74e716d2cf5eb4b918cccf34d2893b
juliettegodyere <juliettejuli@yahoo.com>
2017-02-20 18:11:15 +0100

    first commit

---
+++ client/main.js
@@ -0,0 +1,41 @@
+var myApp = angular.module('myApp', ['ngRoute']);
+
+myApp.config(function ($routeProvider) {
+  $routeProvider
+    .when('/', {
+      templateUrl: 'partials/home.html'
+    })
+    .when('/login', {
+      templateUrl: 'partials/login.html',
+      controller: 'loginController'
+    })
+    .when('/logout', {
+      controller: 'logoutController'
+    })
+    .when('/register', {
+      templateUrl: 'partials/register.html',
+      controller: 'loginController'
+    })
+    .when('/one', {
+      template: '<h1>This is page one!</h1>'
+    })
+    .when('/two', {
+      template: '<h1>This is page two!</h1>'
+    })
+    .otherwise({
+      redirectTo: '/'
+    });
+});
+
+// myApp.run(function ($rootScope, $location, $route, AuthService) {
+//   $rootScope.$on('$routeChangeStart',
+//     function (event, next, current) {
+//       AuthService.getUserStatus()
+//       .then(function(){
+//         if (next.access.restricted && !AuthService.isLoggedIn()){
+//           $location.path('/login');
+//           $route.reload();
+//         }
+//       });
+//   });
+// });
